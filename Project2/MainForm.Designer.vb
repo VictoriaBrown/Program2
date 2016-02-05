582293316513bf39bada5ef99f3e2867a72a5e02
Victoria Brown <victoriabrown0710@gmail.com>
2016-02-04 22:31:12 -0600

    Initial Commit

---
+++ Project2/MainForm.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StateTerritoryListBox = New System.Windows.Forms.ListBox()
+        Me.StateTerritoryTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StateTerritoryListBox
+        '
+        Me.StateTerritoryListBox.FormattingEnabled = True
+        Me.StateTerritoryListBox.Items.AddRange(New Object() {"Alabama", "Alaska", "American", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Trust Territories", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"})
+        Me.StateTerritoryListBox.Location = New System.Drawing.Point(81, 84)
+        Me.StateTerritoryListBox.Name = "StateTerritoryListBox"
+        Me.StateTerritoryListBox.Size = New System.Drawing.Size(184, 95)
+        Me.StateTerritoryListBox.TabIndex = 0
+        '
+        'StateTerritoryTextBox
+        '
+        Me.StateTerritoryTextBox.Location = New System.Drawing.Point(234, 40)
+        Me.StateTerritoryTextBox.Name = "StateTerritoryTextBox"
+        Me.StateTerritoryTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.StateTerritoryTextBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(216, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Please enter or select the U.S. state/territory"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(359, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(359, 225)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StateTerritoryTextBox)
+        Me.Controls.Add(Me.StateTerritoryListBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Program 2 - Victoria Brown"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StateTerritoryListBox As ListBox
+    Friend WithEvents StateTerritoryTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Project2/MainForm.vb
@@ -0,0 +1,287 @@
+﻿Public Class MainForm
+    Private Sub StateTerritoryTextBox_TextChanged(sender As Object, e As EventArgs) Handles StateTerritoryTextBox.TextChanged
+        ' Select Alabama if Alab is entered.
+        If (StateTerritoryTextBox.Text = "Alab" Or StateTerritoryTextBox.Text = "alab") Then
+            StateTerritoryListBox.SelectedIndex = 0
+        End If
+
+        ' Select Alaska if Alas is entered.
+        If (StateTerritoryTextBox.Text = "Alas" Or StateTerritoryTextBox.Text = "alas") Then
+            StateTerritoryListBox.SelectedIndex = 1
+        End If
+
+        ' Select American Samoa if Am is entered.
+        If (StateTerritoryTextBox.Text = "Am" Or StateTerritoryTextBox.Text = "am") Then
+            StateTerritoryListBox.SelectedIndex = 2
+        End If
+
+        ' Select Arizona if Ari is entered.
+        If (StateTerritoryTextBox.Text = "Ari" Or StateTerritoryTextBox.Text = "ari") Then
+            StateTerritoryListBox.SelectedIndex = 3
+        End If
+
+        ' Select Arkansas if Ark is entered.
+        If (StateTerritoryTextBox.Text = "Ark" Or StateTerritoryTextBox.Text = "ark") Then
+            StateTerritoryListBox.SelectedIndex = 4
+        End If
+
+        ' Select California if Ca is entered.
+        If (StateTerritoryTextBox.Text = "Ca" Or StateTerritoryTextBox.Text = "ca") Then
+            StateTerritoryListBox.SelectedIndex = 5
+        End If
+
+        ' Select Colorado if Col is entered.
+        If (StateTerritoryTextBox.Text = "Col" Or StateTerritoryTextBox.Text = "col") Then
+            StateTerritoryListBox.SelectedIndex = 6
+        End If
+
+        ' Select Connecticut if Con is entered.
+        If (StateTerritoryTextBox.Text = "Con" Or StateTerritoryTextBox.Text = "con") Then
+            StateTerritoryListBox.SelectedIndex = 7
+        End If
+
+        ' Select Delaware if De is entered.
+        If (StateTerritoryTextBox.Text = "De" Or StateTerritoryTextBox.Text = "de") Then
+            StateTerritoryListBox.SelectedIndex = 8
+        End If
+
+        ' Select District of Columbia if Di is entered.
+        If (StateTerritoryTextBox.Text = "Di" Or StateTerritoryTextBox.Text = "di") Then
+            StateTerritoryListBox.SelectedIndex = 9
+        End If
+
+        ' Select Florida if F is entered.
+        If (StateTerritoryTextBox.Text = "F" Or StateTerritoryTextBox.Text = "f") Then
+            StateTerritoryListBox.SelectedIndex = 10
+        End If
+
+        ' Select Georgia if Ge is entered.
+        If (StateTerritoryTextBox.Text = "Ge" Or StateTerritoryTextBox.Text = "ge") Then
+            StateTerritoryListBox.SelectedIndex = 11
+        End If
+
+        ' Select Guam if Gu is entered.
+        If (StateTerritoryTextBox.Text = "Gu" Or StateTerritoryTextBox.Text = "gu") Then
+            StateTerritoryListBox.SelectedIndex = 12
+        End If
+
+        ' Select Hawaii if H is entered.
+        If (StateTerritoryTextBox.Text = "H" Or StateTerritoryTextBox.Text = "h") Then
+            StateTerritoryListBox.SelectedIndex = 13
+        End If
+
+        ' Select Idaho if Id is entered.
+        If (StateTerritoryTextBox.Text = "Id" Or StateTerritoryTextBox.Text = "id") Then
+            StateTerritoryListBox.SelectedIndex = 14
+        End If
+
+        ' Select Illinois if Il is entered.
+        If (StateTerritoryTextBox.Text = "Il" Or StateTerritoryTextBox.Text = "il") Then
+            StateTerritoryListBox.SelectedIndex = 15
+        End If
+
+        ' Select Indiana if In is entered.
+        If (StateTerritoryTextBox.Text = "In" Or StateTerritoryTextBox.Text = "in") Then
+            StateTerritoryListBox.SelectedIndex = 16
+        End If
+
+        ' Select Iowa if Io is entered.
+        If (StateTerritoryTextBox.Text = "Io" Or StateTerritoryTextBox.Text = "io") Then
+            StateTerritoryListBox.SelectedIndex = 17
+        End If
+
+        ' Select Kansas if Ka is entered.
+        If (StateTerritoryTextBox.Text = "Ka" Or StateTerritoryTextBox.Text = "ka") Then
+            StateTerritoryListBox.SelectedIndex = 18
+        End If
+
+        ' Select Kentucky if Ke is entered.
+        If (StateTerritoryTextBox.Text = "Ke" Or StateTerritoryTextBox.Text = "ke") Then
+            StateTerritoryListBox.SelectedIndex = 19
+        End If
+
+        ' Select Louisiana if L is entered.
+        If (StateTerritoryTextBox.Text = "L" Or StateTerritoryTextBox.Text = "l") Then
+            StateTerritoryListBox.SelectedIndex = 20
+        End If
+
+        ' Select Maine if Mai is entered.
+        If (StateTerritoryTextBox.Text = "Mai" Or StateTerritoryTextBox.Text = "mai") Then
+            StateTerritoryListBox.SelectedIndex = 21
+        End If
+
+        ' Select Maryland if Mar is entered.
+        If (StateTerritoryTextBox.Text = "Mar" Or StateTerritoryTextBox.Text = "mar") Then
+            StateTerritoryListBox.SelectedIndex = 22
+        End If
+
+        ' Select Massachusetts if Mas is entered.
+        If (StateTerritoryTextBox.Text = "Mas" Or StateTerritoryTextBox.Text = "mas") Then
+            StateTerritoryListBox.SelectedIndex = 23
+        End If
+
+        ' Select Michigan if Mic is entered.
+        If (StateTerritoryTextBox.Text = "Mic" Or StateTerritoryTextBox.Text = "mic") Then
+            StateTerritoryListBox.SelectedIndex = 24
+        End If
+
+        ' Select Minnesota if Min is entered.
+        If (StateTerritoryTextBox.Text = "Min" Or StateTerritoryTextBox.Text = "min") Then
+            StateTerritoryListBox.SelectedIndex = 25
+        End If
+
+        ' Select Mississippi if Missi is entered.
+        If (StateTerritoryTextBox.Text = "Missi" Or StateTerritoryTextBox.Text = "missi") Then
+            StateTerritoryListBox.SelectedIndex = 26
+        End If
+
+        ' Select Missouri if Misso is entered.
+        If (StateTerritoryTextBox.Text = "Misso" Or StateTerritoryTextBox.Text = "misso") Then
+            StateTerritoryListBox.SelectedIndex = 27
+        End If
+
+        ' Select Montana if Mo is entered.
+        If (StateTerritoryTextBox.Text = "Mo" Or StateTerritoryTextBox.Text = "mo") Then
+            StateTerritoryListBox.SelectedIndex = 28
+        End If
+
+        ' Select Nebraska if Neb is entered.
+        If (StateTerritoryTextBox.Text = "Neb" Or StateTerritoryTextBox.Text = "neb") Then
+            StateTerritoryListBox.SelectedIndex = 29
+        End If
+
+        ' Select Nevada if Nev is entered.
+        If (StateTerritoryTextBox.Text = "Nev" Or StateTerritoryTextBox.Text = "nev") Then
+            StateTerritoryListBox.SelectedIndex = 30
+        End If
+
+        ' Select New Hampshire if New H is entered.
+        If (StateTerritoryTextBox.Text = "New H" Or StateTerritoryTextBox.Text = "new h") Then
+            StateTerritoryListBox.SelectedIndex = 31
+        End If
+
+        ' Select New Jersey if New J is entered.
+        If (StateTerritoryTextBox.Text = "New J" Or StateTerritoryTextBox.Text = "new j") Then
+            StateTerritoryListBox.SelectedIndex = 32
+        End If
+
+        ' Select New Mexico if New M is entered.
+        If (StateTerritoryTextBox.Text = "New M" Or StateTerritoryTextBox.Text = "new m") Then
+            StateTerritoryListBox.SelectedIndex = 33
+        End If
+
+        ' Select New York if New York is entered.
+        If (StateTerritoryTextBox.Text = "New Y" Or StateTerritoryTextBox.Text = "new y") Then
+            StateTerritoryListBox.SelectedIndex = 34
+        End If
+
+        ' Select North Carolina if North C is entered.
+        If (StateTerritoryTextBox.Text = "North C" Or StateTerritoryTextBox.Text = "north c") Then
+            StateTerritoryListBox.SelectedIndex = 35
+        End If
+
+        ' Select North Dakota if North D is entered.
+        If (StateTerritoryTextBox.Text = "North D" Or StateTerritoryTextBox.Text = "north d") Then
+            StateTerritoryListBox.SelectedIndex = 36
+        End If
+
+        ' Select Ohio if Oh is entered.
+        If (StateTerritoryTextBox.Text = "Oh" Or StateTerritoryTextBox.Text = "oh") Then
+            StateTerritoryListBox.SelectedIndex = 37
+        End If
+
+        ' Select Oklahoma if Ok is entered.
+        If (StateTerritoryTextBox.Text = "Ok" Or StateTerritoryTextBox.Text = "ok") Then
+            StateTerritoryListBox.SelectedIndex = 38
+        End If
+
+        ' Select Oregon if Or is entered.
+        If (StateTerritoryTextBox.Text = "Or" Or StateTerritoryTextBox.Text = "or") Then
+            StateTerritoryListBox.SelectedIndex = 39
+        End If
+
+        ' Select Pennsylvania if Pe is entered.
+        If (StateTerritoryTextBox.Text = "Pe" Or StateTerritoryTextBox.Text = "pe") Then
+            StateTerritoryListBox.SelectedIndex = 40
+        End If
+
+        ' Select Puerto Rico if Pu is entered.
+        If (StateTerritoryTextBox.Text = "Pu" Or StateTerritoryTextBox.Text = "pu") Then
+            StateTerritoryListBox.SelectedIndex = 41
+        End If
+
+        ' Select Rhode Island if R is entered.
+        If (StateTerritoryTextBox.Text = "R" Or StateTerritoryTextBox.Text = "r") Then
+            StateTerritoryListBox.SelectedIndex = 42
+        End If
+
+        ' Select South Carolina if South C is entered.
+        If (StateTerritoryTextBox.Text = "South C" Or StateTerritoryTextBox.Text = "south c") Then
+            StateTerritoryListBox.SelectedIndex = 43
+        End If
+
+        ' Select South Dakota if South D is entered.
+        If (StateTerritoryTextBox.Text = "South D" Or StateTerritoryTextBox.Text = "south d") Then
+            StateTerritoryListBox.SelectedIndex = 44
+        End If
+
+        ' Select Tennessee if Ten is entered.
+        If (StateTerritoryTextBox.Text = "Ten" Or StateTerritoryTextBox.Text = "ten") Then
+            StateTerritoryListBox.SelectedIndex = 45
+        End If
+
+        ' Select Texas if Tex is entered.
+        If (StateTerritoryTextBox.Text = "Tex" Or StateTerritoryTextBox.Text = "tex") Then
+            StateTerritoryListBox.SelectedIndex = 46
+        End If
+
+        ' Select Trusted Territories if Tr is entered.
+        If (StateTerritoryTextBox.Text = "Tr" Or StateTerritoryTextBox.Text = "tr") Then
+            StateTerritoryListBox.SelectedIndex = 47
+        End If
+
+        ' Select Utah if U is entered.
+        If (StateTerritoryTextBox.Text = "U" Or StateTerritoryTextBox.Text = "u") Then
+            StateTerritoryListBox.SelectedIndex = 48
+        End If
+
+        ' Select Vermont if Ve is entered.
+        If (StateTerritoryTextBox.Text = "Ve" Or StateTerritoryTextBox.Text = "ve") Then
+            StateTerritoryListBox.SelectedIndex = 49
+        End If
+
+        ' Select Virgin Islands if Virgin I is entered.
+        If (StateTerritoryTextBox.Text = "Virgin I" Or StateTerritoryTextBox.Text = "virgin i") Then
+            StateTerritoryListBox.SelectedIndex = 50
+        End If
+
+        ' Select Virginia if Virgini is entered.
+        If (StateTerritoryTextBox.Text = "Virgini" Or StateTerritoryTextBox.Text = "virgini") Then
+            StateTerritoryListBox.SelectedIndex = 51
+        End If
+
+        ' Select Washington if Wa is entered.
+        If (StateTerritoryTextBox.Text = "Wa" Or StateTerritoryTextBox.Text = "wa") Then
+            StateTerritoryListBox.SelectedIndex = 52
+        End If
+
+        ' Select West Virginia if We is entered.
+        If (StateTerritoryTextBox.Text = "We" Or StateTerritoryTextBox.Text = "we") Then
+            StateTerritoryListBox.SelectedIndex = 53
+        End If
+
+        ' Select Wisconsin if Wi is entered.
+        If (StateTerritoryTextBox.Text = "Wi" Or StateTerritoryTextBox.Text = "wi") Then
+            StateTerritoryListBox.SelectedIndex = 54
+        End If
+
+        ' Select Wyoming if Wy is entered.
+        If (StateTerritoryTextBox.Text = "Wy" Or StateTerritoryTextBox.Text = "wy") Then
+            StateTerritoryListBox.SelectedIndex = 55
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+End Class
